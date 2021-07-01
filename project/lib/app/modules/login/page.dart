48d39b02f6e77fefb61d71ac0c2dd6d70d4c0125
Carlos Castro <carloscastrogames@gmail.com>
2021-06-30 21:31:19 -0300

    Revert "inserindo imagens"
    This reverts commit bf7d83f7e58343c12f8fcba3fe0be2cb25a113f5.

--- project/lib/app/modules/login/page.dart
+++ project/lib/app/modules/login/page.dart
@@ -10,48 +10,9 @@ class LoginScreem extends StatelessWidget {
           Container(
             height: 400,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                      'assets/background.png',
-                    ),
-                    fit: BoxFit.fill)),
-            child: Stack(
-              children: [
-                Positioned(
-                    left: 30,
-                    width: 80,
-                    height: 200,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                      image: AssetImage(
-                        'assets/light-1.png',
-                      ),
-                    )))),
-                Positioned(
-                    left: 140,
-                    width: 80,
-                    height: 150,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                      image: AssetImage(
-                        'assets/light-2.png',
-                      ),
-                    )))),
-                Positioned(
-                    right: 40,
-                    top: 40,
-                    width: 80,
-                    height: 150,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                      image: AssetImage(
-                        'assets/clock.png',
-                      ),
-                    )))),
-              ],
+              image: DecorationImage(
+                image: AssetImage('')
+              )
             ),
           )
         ],
